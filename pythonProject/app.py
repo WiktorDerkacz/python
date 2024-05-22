e1ca133d26dde991542eaf843b8a49e1651062a5
WiktorDerkacz <wiktor.derkacz03@gmail.com>
2024-05-22 14:16:57 +0200

    Add files via upload

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, url_for
+from pip._internal.vcs import git
+
 app = Flask(__name__)
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./python')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '',200
+
 @app.route('/')
 def index():
     return render_template('index.html')
